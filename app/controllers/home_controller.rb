312d3a0fc903113da6ad44b41cea3c6260da9ff4
callumahern <callum.ahern@simplybusiness.co.uk>
2022-11-21 11:41:44 +0000

    entry styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   
   ENTRY_MODELS = [Food, Finance, Mood, Habits]
+  
   def homepage
     @entries = ENTRY_MODELS.flat_map { |model| model.where(entry_date: DateTime.now.beginning_of_month.beginning_of_week..DateTime.now.end_of_month.end_of_week) }
   end
