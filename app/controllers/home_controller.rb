9ef0680e72a40d9b77cb08f264be7394452b82f1
callumahern <callum.ahern@simplybusiness.co.uk>
2022-11-21 11:10:36 +0000

    referenced all models in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
+  
+  ENTRY_MODELS = [Food, Finance, Mood, Habits]
   def homepage
-    
+    @entries = ENTRY_MODELS.flat_map { |model| model.where(entry_date: DateTime.now.beginning_of_month.beginning_of_week..DateTime.now.end_of_month.end_of_week) }
   end
+
 end
